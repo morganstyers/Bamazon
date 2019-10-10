021c3158110237fcf384e2b2746edc65bc7d3052
Morgan Styers <morganstyers@gmail.com>
2019-10-10 02:02:53 -0400

    final tweaks

--- bamazon.sql
+++ bamazon.sql
@@ -1,21 +1,2 @@
-DROP DATABASE if EXISTS bamazon_DB;
-CREATE database bamazon_DB;
-USE bamazon_DB;
 
-CREATE TABLE products (
-	item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
-	product_name VARCHAR(30) NOT NULL,
-	department_name VARCHAR(20) NOT NULL,
-	price DECIMAL(10,2) NOT NULL,
-	stock_quantity INTEGER(11) NOT NULL,
-	PRIMARY KEY (item_id)
-);
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES  ('Item', 'Cosmetics', 8.00, 30),
-		('Item', 'Cosmetics', 6.25, 200),
-		('Item', 'Grocery', 5.34, 9),
-		('Item', 'Grocery', 1.00, 1),
-		('Item', 'Produce', 0.35, 2),
-		('Item', 'Produce', 0.20, 900),
-		('Item', 'Grocery', 4.45, 26)
-		
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,6 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
 
-
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -10,72 +9,113 @@ var connection = mysql.createConnection({
     database: "bamazon_DB"
 });
 
-inquirer.prompt([
-    {
-        type: 'input',
-        name: 'user',
-        message: "Hello! Please enter your username:",
+function app() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'user',
+            message: "Hello! Please enter your username:",
 
-    }]).then(name => {
-        const user = name.user;
-        console.log("----------------------------------------------------------" + "\n" + "Hello, " + user)
+        }]).then(name => {
 
-        function next() {
-            console.log(" lets show you the inventory, just press enter when you're done looking and ready to check out!" + "\n" + "\n"),
+            const user = name.user;
 
-                setTimeout(function () { console.log("\n") + inventory() + "\n" }, 2000);
+            console.log("----------------------------------------------------------" + "\n" + "Hello, " + user)
+            console.log(" lets show you the inventory, just press enter when you're done looking and ready to check out!" + "\n" + "\n");
 
-            inquirer.prompt([
-                {
-                    type: "confirm",
-                    name: 'continue',
-                    message: "ready?"
-                },
-                {
-                    type: 'input',
+            function start() {
+                inquirer.prompt([
+                    {
+                        name: 'continue',
+                        message: '\n' + '\n------------------------------------------------------------\n' + '\n'
+                    },
+                    {
+                        name: "checkout? hit enter",
+                        message: inventory()
+                    },
+                    {
+                        type: 'input',
+                        name: 'item_id',
+                        message: name.user + " ," + " please enter the Item ID which you would like to purchase.",
+                        filter: Number
+                    },
+                    {
+                        type: 'input',
+                        name: 'quantity',
+                        message: 'How many do you need?',
+                        filter: Number
 
-                    name: 'item_id',
-                    message: name.user + " ," + " please enter the Item ID which you would like to purchase.",
-                    filter: Number
-                },
-                {
-                    type: 'input',
-                    name: 'quantity',
-                    message: 'How many do you need?',
-                    filter: Number
+                    }]).then(answers => {
 
-                }]).then(input => {
+                        var item = answers.item_id;
+                        var quantity = answers.quantity;
 
-                    var item = input.item_id;
-                    var quantity = input.quantity;
+                        var queryStr = 'SELECT * FROM products WHERE ?';
 
-                    var queryStr = 'SELECT * FROM products WHERE ?';
 
-                    connection.query(queryStr, { item_id: item }, function (err, data) {
-                        if (err) throw err;
-                        if (data.length === 0) {
-                            console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
-                        }
+                        connection.query(queryStr, { item_id: item }, function (err, data) {
+                            if (err) {
+                                return (err)
+                            }
+
+                            else if (data.length === 0) {
+                                console.log('ERROR: Invalid Item ID. Please review the inventory and select a valid Item ID.');
+                                inventory();
+                            } else {
+                                var products = data[0];
+                            }
+                            if (quantity <= products.stock_quantity) {
+                                console.log("---------------------------------------------------------------------\n" + '\n');
+                                console.log("Excellent choice, " + user + "! Proceeding to checkout." + '\n');
+                                console.log("---------------------------------------------------------------------\n" + '\n');
+
+
+                                var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (products.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+                                connection.query(updateQueryStr, function (err, data) {
+                                    if (err) throw err;
+                                    console.log('Your order has been sucessfully placed!' + '\n' + '\n' + 'Your total is $' + products.price * quantity)
+                                    console.log('\n');
+
+                                    console.log("thank you, " + user + "!!")
+                                    console.log("---------------------------------------------------------------------\n");
+
+                                    connection.end();
+                                });
+                            } else {
+                                console.log('Sorry, that item is all out of stock!' + '\n');
+
+                                inventory();
+                                start();
+                            }
+                        })
                     })
-                })
-        }
-    });
+
+            }
+            start();
+
+        })
+}
 
 
 function inventory() {
-    queryStr = 'SELECT * FROM products';
-    connection.query(queryStr, function (err, data) {
-        if (err) throw err;
-        var pin = '';
-        for (var i = 0; i < data.length; i++) {
-            pin = '';
-            pin += 'Item ID: ' + data[i].item_id + '  //  ';
-            pin += 'Product Name: ' + data[i].product_name + '  //  ';
-            pin += 'Department: ' + data[i].department_name + '  //  ';
-            pin += 'Price: $' + data[i].price + '\n';
+    setTimeout(function () {
+        queryStr = 'SELECT * FROM products';
+        connection.query(queryStr, function (err, data) {
+            if (err) throw err;
+            var pin = '';
+            for (var i = 0; i < data.length; i++) {
+                pin = '';
+                pin += 'Item ID: ' + data[i].item_id + '  //  ';
+                pin += 'Product Name: ' + data[i].product_name + '  //  ';
+                pin += 'Department: ' + data[i].department_name + '  //  ';
+                pin += 'Price: $' + data[i].price + '\n';
+                console.log(pin);
+            }
+        })
+    }, 2000)
+    console.log("---------------------------------------------------------------------\n");
 
-            console.log(pin);
-
-        }
-    })
 }
+
+app();
+
